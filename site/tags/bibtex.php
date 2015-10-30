02c8533485fa7f860991d34af01526d82183cb75
Philipp Mundhenk <philipp@mundhenk.org>
2015-10-30 17:54:12 +0800

    license added shields added

--- site/tags/bibtex.php
+++ site/tags/bibtex.php
@@ -7,8 +7,8 @@ kirbytext::$tags['bibtex'] = array(
 	$html .= '<div class=publications>';
     $html .= '<table id=pubTable class=display></table>';
 	$html .= '</div>';
-	$html .= '<script type=text/javascript src=assets/bibtex/jquery.min.js></script>';
-	$html .= '<script type=text/javascript src=assets/bibtex/jquery.dataTables.min.js></script>';
+	$html .= '<script type=text/javascript src=https://code.jquery.com/jquery-1.6.4.min.js></script>';
+	$html .= '<script type=text/javascript src=https://cdn.datatables.net/1.6.2/js/jquery.dataTables.min.js></script>';
 	$html .= '<script type=text/javascript src=assets/bibtex/BibTex-0.1.2.js></script>';
 	$html .= '<script type=text/javascript src=assets/bibtex/bib-publication-list.js></script>';
 	$html .= '<script type=text/javascript>';
